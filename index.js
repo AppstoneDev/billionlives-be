3f94754dae89e97b0e4e5023d62c8fd29f9ad1ca
Saraschandraa Muralisekar <saraschandraa.naidu@hotmail.com>
2022-06-10 14:21:07 +0530

    Implementation of GET, POST, PUT and DELETE METHODS 10Jun2022

--- index.js
+++ index.js
@@ -2,22 +2,99 @@ const express = require('express')
 const app = express();
 
 
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
-app.listen("8000", ()=>{
+app.listen("8000", () => {
   console.log("PORT is running on 8000");
 })
 
-app.get("/api/welcome", (req, res)=>{
+app.get("/api/welcome", (req, res) => {
   // res.send("Billion Lives Backend Project")
-  if(req.query.username== "Saras" && req.query.password == "qwerty"){
-    var json ={"name" : "Saras", "mobile" : "99999999"}; 
+  if (req.query.username == "Saras" && req.query.password == "qwerty") {
+    var json = { "name": "Saras", "mobile": "99999999" };
     // res.send("Login successful");
-    res.json({"status": true, "message": "Login Succesful", result: json});
-  }else{
+    res.json({ "status": true, "message": "Login Succesful", result: json });
+  } else {
     res.send("Invalid Credentials");
   }
 })
 
+
+var users = [];
+
+var id = 0;
+
+app.post("/api/user", (req, res) => {
+  var name = req.body.name;
+  var email = req.body.email;
+  var mob = req.body.mob_no;
+  var password = req.body.password;
+
+  if (users.length > 0) {
+    for (var user of users) {
+      if (user.email == email) {
+        res.json({ status: false, message: "Email already exists" });
+        return;
+      }
+    }
+  }
+
+  var newUser = {
+    "id" : id,
+    "name": name,
+    "email": email,
+    "mob": mob,
+    "password": password,
+  }
+  id++
+  users.push(newUser);
+  res.json({ status: true, message: "User added successfully" })
+})
+
+app.get("/api/user", (req, res) => {
+
+  var name= req.query.name;
+  if(name != undefined && name != null && name != "" ){
+    for (var user of users){
+      if(user.name == req.query.name){
+        res.json({status:true, message:"user detail found", result : user});
+        return;
+      }
+    }
+    res.json({status:false, message:"There is no any such user"});
+  }else{
+    res.json({status:true, message:"User details", result : users});
+  }
+})
+
+
+app.put("/api/user", (req, res)=>{
+  var id = parseInt(req.body.id)
+  var email = req.body.email
+  var name = req.body.name
+  var password = req.body.password
+  var mob = req.body.mob_no
+
+  for (var user of users){
+    if(user.id == id){
+      var updatedUser = {
+        id : id,
+        "email" : email,
+        "name" : name,
+        "password" : password,
+        "mob" : mob
+      }
+
+      users.pop(user);
+      users.push(updatedUser);
+      res.json({status:true, message:"user data updated"});
+      return
+    }
+  }
+  
+  res.json({status:false, message:"invalid user id"});
+})
 
 //GET -> 
 //POST -> Data addition
@@ -26,3 +103,5 @@ app.get("/api/welcome", (req, res)=>{
 //DELETE -> for data deletion
 
 //Assignment 1 - Create a Login API and implement validations. 
+
+//Assignment 2 - Create APIs for the UI implemented in the React Project and map it to the UI. 
