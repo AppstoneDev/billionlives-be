1c4323f85769eab57f9c3c5e34ab1a8cc283b1b1
Saraschandraa Muralisekar <saraschandraa.naidu@hotmail.com>
2022-06-20 13:33:09 +0530

    MY SQL DATABASE Connector 20June2022

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const app = express();
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.listen("8000", () => {
-  console.log("PORT is running on 8000");
+var dbConnector = require("./dbconnector");
+
+dbConnector.connectToDB(() => {
+  app.listen("8000", () => {
+    console.log("PORT is running on 8000");
+  })
 })
+
+
 
 var secret = "841235432344ADRA";
 let crypto = require('crypto-js');
@@ -36,75 +42,91 @@ app.post("/api/user", (req, res) => {
   var mob = req.body.mob_no;
   var password = req.body.password;
 
-  if (users.length > 0) {
-    for (var user of users) {
-      if (user.email == email) {
-        res.json({ status: false, message: "Email already exists" });
-        return;
-      }
-    }
-  }
+  // if (users.length > 0) {
+  //   for (var user of users) {
+  //     if (user.email == email) {
+  //       res.json({ status: false, message: "Email already exists" });
+  //       return;
+  //     }
+  //   }
+  // }
 
-  var newUser = {
-    "id" : id,
-    "name": name,
-    "email": email,
-    "mob": mob,
-    "password": password,
-  }
-  id++
-  users.push(newUser);
-  res.json({ status: true, message: "User added successfully" })
+  // var newUser = {
+  //   "id": id,
+  //   "name": name,
+  //   "email": email,
+  //   "mob": mob,
+  //   "password": password,
+  // }
+  // id++
+  // users.push(newUser);
+
+  // var query = "INSERT INTO USER(name, email, password, username, mobile) VALUES ('" + name + "','" + email + "','" + password + "',''," + mob + ")";
+
+
+  var query  = "SELECT * FROM USER";
+
+  dbConnector.perfromDBOperation(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.json({ status: false, message: "User not added successfully" })
+    } else {
+      console.log(result);
+      res.json({ status: true, message: "User added successfully" })
+    }
+  })
+
+
 })
 
 app.get("/api/user", (req, res) => {
 
-  var name= req.query.name;
-  if(name != undefined && name != null && name != "" ){
-    for (var user of users){
-      if(user.name == req.query.name){
-        res.json({status:true, message:"user detail found", result : user});
+  var name = req.query.name;
+  if (name != undefined && name != null && name != "") {
+    for (var user of users) {
+      if (user.name == req.query.name) {
+        res.json({ status: true, message: "user detail found", result: user });
         return;
       }
     }
-    res.json({status:false, message:"There is no any such user"});
-  }else{
-    res.json({status:true, message:"User details", result : users});
+    res.json({ status: false, message: "There is no any such user" });
+  } else {
+    res.json({ status: true, message: "User details", result: users });
   }
 })
 
 
-app.put("/api/user", (req, res)=>{
+app.put("/api/user", (req, res) => {
   var id = parseInt(req.body.id)
   var email = req.body.email
   var name = req.body.name
   var password = req.body.password
   var mob = req.body.mob_no
 
-  for (var user of users){
-    if(user.id == id){
+  for (var user of users) {
+    if (user.id == id) {
       var updatedUser = {
-        id : id,
-        "email" : email,
-        "name" : name,
-        "password" : password,
-        "mob" : mob
+        id: id,
+        "email": email,
+        "name": name,
+        "password": password,
+        "mob": mob
       }
 
       users.pop(user);
       users.push(updatedUser);
-      res.json({status:true, message:"user data updated"});
+      res.json({ status: true, message: "user data updated" });
       return
     }
   }
-  
-  res.json({status:false, message:"invalid user id"});
+
+  res.json({ status: false, message: "invalid user id" });
 })
 
 
-app.post("/login", decrypter.decryptBody, (req, res)=>{
+app.post("/login", decrypter.decryptBody, (req, res) => {
   console.log("body", req.body);
-  res.json({status:true, message: "decryptor API called" });
+  res.json({ status: true, message: "decryptor API called" });
 })
 
 //GET -> 
@@ -118,3 +140,5 @@ app.post("/login", decrypter.decryptBody, (req, res)=>{
 //Assignment 2 - Create APIs for the UI implemented in the React Project and map it to the UI. 
 
 //Assignment 3 - Create a postman environment to send the payload as decrypted data for POST and PUT method only.
+
+//Assignment 4 - Create a new MySQL database. And replace the API methods implemented using local array to Database.
